5e294f6a2eb50ba535230a2d43bbd0c825b3f634
cereceres <jesus.edelcereceres@gmail.com>
2017-11-30 18:42:02 -0600

    the test are fixed

--- test/index.js
+++ test/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
 const assert = require('assert');
-const { requireAll } = require('../index');
+const { requireAll, requireAllSync } = require('../index');
 describe('Test to requireall', () => {
     it('should esport a function', () => {
         assert(typeof requireAll === 'function');
     });
 
-    it('should esport a function', (done) => requireAll('../test/testing', (err, allFiles) => {
+    it('should export a funcition what get all files', (done) => requireAll('../test/testing', (err, allFiles) => {
         assert(!err);
-        assert(Array.isArray(allFiles));
+        assert.deepEqual(allFiles, [ 1, 'test2' ]);
         done();
     }));
+    it('should return a promise if cb is not given', () => requireAll('../test/testing').then((allFiles) => {
+        assert.deepEqual(allFiles, [ 1, 'test2' ]);
+    }));
+
+    it('should export a sync version', () => {
+        const allFiles = requireAllSync('../test/testing');
+        assert.deepEqual(allFiles, [ 1, 'test2' ]);
+    });
 });
